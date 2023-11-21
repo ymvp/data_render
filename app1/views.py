7da691ad6cd7b27c6e39f1edd882bc44545b8e1a
ymvp <ymvp76@gmail.com>
2023-11-21 15:48:57 +0530

    first commit

--- app1/views.py
+++ app1/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def data_render(request):
-    data = 'imagination of data from database goes from backend to front end'
-    d ={'talent': data}
-    return render(request, 'data_render.html', context= d)
+    data ="imagination of data from database goes from backend to front end"
+    d ={'app':data}
+    return render(request, 'data_render.html',context=d)
